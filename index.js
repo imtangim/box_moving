fc23898f464b0442385139c69548d5757ba3ee5e
shadow_ghost <tanjim437@gmail.com>
2023-03-19 05:24:26 +0600

    fixed

--- index.js
+++ index.js
@@ -3,17 +3,14 @@ const mydiv = document.getElementsByClassName("mydiv")
 
 try {
     document.createEvent("TouchEvent")
-    let p = document.getElementById("title1")
-    let p1 = document.getElementById("title2")
-    p1.style.display = "none"
-
-
-    
+    let p1 = document.getElementById("title1")
+    let p2 = document.getElementById("title2")
+    p2.style.display = "none"
     mydiv[0].addEventListener("touchstart", e => {
         e.preventDefault()
             ;[...e.changedTouches].forEach(touch => {
                 mydiv[0].id = touch.identifier
-                p.style.display = "none"
+                p1.style.display = "none"
             })
 
     })
@@ -30,16 +27,16 @@ try {
                 const dot = document.getElementById(touch.identifier)
                 mydiv[0].style.top = `${touch.pageY}px`
                 mydiv[0].style.left = `${touch.pageX}px`
-                p.style.display = "block"
+                p1.style.display = "block"
             })
 
     })
 
 } catch (error) {
-    let p = document.getElementById("title1")
-    let p1 = document.getElementById("title2")
-    p.style.display = "none"
-    p1.style.display = "block"
+    let p1 = document.getElementById("title1")
+    let p2 = document.getElementById("title2")
+    p1.style.display = "none"
+    p2.style.display = "block"
     window.addEventListener("keydown", move)
     let x = 0
     let y = 0
@@ -72,36 +69,3 @@ try {
         p1.style.display = "none"
     }
 }
-
-
-
-
-
-// touch code
-// document.addEventListener("touchstart", e => {
-//     ;[...e.changedTouches].forEach(touch => {
-//         const dot = document.createElement("div")
-//         dot.classList.add("dot")
-//         dot.style.top = `${touch.pageY}px`
-//         dot.style.left = `${touch.pageX}px`
-//         dot.style.backgroundColor = "red"
-
-//         dot.id = touch.identifier
-//         document.body.append(dot)
-//     })
-// })
-// document.addEventListener("touchmove", e => {
-//     ;[...e.changedTouches].forEach(touch => {
-//         const dot = document.getElementById(touch.identifier)
-//         dot.style.top = `${touch.pageY}px`
-//         dot.style.left = `${touch.pageX}px`
-//     })
-// })
-// document.addEventListener("touchend", e => {
-//     console.log("end")
-//         ;[...e.changedTouches].forEach(touch => {
-//             const dot = document.getElementById(touch.identifier)
-//             dot.remove()
-//         })
-
-// })
